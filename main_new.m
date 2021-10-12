8b6224f5339b670abd812e8b73a6723ee3f9c311
rasskazovpavel <43547046+rasskazovpavel@users.noreply.github.com>
2021-10-12 17:09:13 +0300

    Added single timeline

--- main_new.m
+++ main_new.m
@@ -28,7 +28,7 @@ wgs84 = wgs84Ellipsoid('meter');
 
 %Моделируем ЛЧМ сигнал
 timeStart = 0; %время начала моделирования
-timeFinish = timeStart + 0.1; %время окончания моделирования
+timeFinish = timeStart + 0.0001; %время окончания моделирования
 periodEmit = 3 * 10^(-4); %период между импульсами
 amplitude = 10 ^ 50; %амплитуда сигнала
 frequencyStart = 0; %начальная частота сигнала
@@ -36,26 +36,21 @@ frequencyEnd = 10^8; %конечная частота сигнала
 timeImpulse = 10^(-4); %длительность импульса
 timesEmit = timeStart:periodEmit:timeFinish; %времена излучения импульсов
 times = 0:5*10^(-9):0+timeFinish; %период дискретизации для сигнала
-timesDistance = 0:3*10^(-4):0+timeFinish; %период дискретизации для расстояний
-timesTau = 0:4*10^(-6):0+timeFinish; %период дискретизации для задержки
+timesDelay = times; %времена принятия сигнала с учётом задержки
+%timesDistance = 0:3*10^(-4):0+timeFinish; %период дискретизации для расстояний
+%timesTau = 0:4*10^(-6):0+timeFinish; %период дискретизации для задержки
 
-delays = taus(timesTau, receiverGeocen, objectGeocen, objectVelocity) %массив значений задержки
-count = 1;
+delays = taus(times, receiverGeocen, objectGeocen, objectVelocity); %массив значений задержки
 
 %находим в цикле для каждого отсчёта времени для сигнала соответствующую задержку
 for i = 1:length(times)
-    if count < length(timesTau)
-        if times(i) >= timesTau(count + 1)
-            count = count + 1;
-        end
-    end
-    times(i) = times(i) + delays(count); %прибавляем задержку к времени отсчёта сигнала
+    timesDelay(i) = times(i) + delays(i); %прибавляем задержку к времени отсчёта сигнала
 end
 
-distancesArray = distances(timesDistance, receiverGeocen, objectGeocen, objectVelocity); %массив значений расстояния между спутником и приёмником
+distancesArray = distances(times, receiverGeocen, objectGeocen, objectVelocity); %массив значений расстояния между спутником и приёмником
 coefficients = 1 ./ (4 * pi * distancesArray); %массив значений коэффициента ослабления из-за расстояния
-coefficients = coefficients .* objectWeakening(VObject, receiverGeocen, objectGeocen, objectVelocity, timesDistance); %добавляем коэффициент ослабления из-за диаграммы направленности объекта
-coefficients = coefficients .* receiverWeakening(VReceiver, receiverGeocen, objectGeocen, objectVelocity, timesDistance, wgs84); %добавляем коэффициент ослабления из-за диаграммы направленности приёмника
+coefficients = coefficients .* objectWeakening(VObject, receiverGeocen, objectGeocen, objectVelocity, times); %добавляем коэффициент ослабления из-за диаграммы направленности объекта
+coefficients = coefficients .* receiverWeakening(VReceiver, receiverGeocen, objectGeocen, objectVelocity, times, wgs84); %добавляем коэффициент ослабления из-за диаграммы направленности приёмника
 
 omega = 1 * 10 ^ (-3); %водность в облаках
 lambda = 3 * 10 ^ (-2); %длина волны
@@ -65,21 +60,16 @@ b = 1.31; %эмпирическая величина
 cloudThickness = 10; %толщина облаков
 rainThickness = 10; %толщина дождевого слоя
 
-atmosphereCoefficients = atmosphereWeakening(timesDistance, omega, lambda, a, I, b, cloudThickness, rainThickness); %коэффициент ослабления из-за атмосферы
+atmosphereCoefficients = atmosphereWeakening(times, omega, lambda, a, I, b, cloudThickness, rainThickness); %коэффициент ослабления из-за атмосферы
 coefficients = coefficients .* atmosphereCoefficients; %обновляем значения массива коэффициентов
 
-signalTimes = acceptedSignal_new(amplitude, frequencyStart, frequencyEnd, timeImpulse, times, periodEmit); %задаём массив значений сигнала
+signalTimes = acceptedSignal_new(amplitude, frequencyStart, frequencyEnd, timeImpulse, timesDelay, periodEmit); %задаём массив значений сигнала
 times = 0:5*10^(-9):0+timeFinish; %задаём массив отсчётов времени для сигнала
 count = 1;
 
 %находим в цикле для каждого отсчёта времени для сигнала соответствующее значение коэффициента ослабления
 for i = 1:length(times)
-    if count < length(timesDistance)
-        if times(i) >= timesDistance(count + 1)
-            count = count + 1;
-        end
-    end
-    signalTimes(i) = signalTimes(i) * coefficients(count); %умножаем значение сигнала на соответствующий коэффициент
+    signalTimes(i) = signalTimes(i) * coefficients(i); %умножаем значение сигнала на соответствующий коэффициент
 end
 
-%plot(times, signalTimes);
+plot(times, signalTimes);
--- taus.m
+++ taus.m
@@ -1,9 +1,9 @@
 %Функция, вычисляющая значения задержки сигнала в зависимости от сигнала
-function delays = taus(timesTau,receiverGeocen, objectGeocen, objectVelocity)
+function delays = taus(times,receiverGeocen, objectGeocen, objectVelocity)
     c = 3 * 10^8; %скорость света
-    delays = zeros(1, length(timesTau)); %объявляем массив значений задержки
+    delays = zeros(1, length(times)); %объявляем массив значений задержки
     count = 1;
-    for time = timesTau
+    for time = times
         delays(count) = sqrt(sum((receiverGeocen - objectGeocen - objectVelocity * time) .^ 2)) / c; %ищем значение задержки
         count = count + 1;
     end
